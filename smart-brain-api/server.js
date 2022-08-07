894a6db62ea179fe354e7f708ee8fbb8b84431c3
Stephan DuVal <stephan.duval@gmail.com>
2022-08-07 10:53:08 -0700

    finished Lesson 299

---
+++ smart-brain-api/server.js
@@ -0,0 +1,106 @@
+// import 'express'
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// create app using express
+const app = express();
+app.use(bodyParser.json());
+
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "Johan",
+      email: "johan@gmail.com",
+      password: "cookies",
+      entries: "0",
+      joined: new Date(),
+    },
+    {
+      id: "122",
+      name: "Gill",
+      email: "Gilly@gmail.com",
+      password: "spookies",
+      entries: "0",
+      joined: new Date(),
+    },
+  ],
+};
+
+// A basic route to make sure everything is working we are usint '/' the route
+// we check the server using postman at localhost:3000
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
+
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("Sucess");
+  } else {
+    res.status(400).json("error logging in");
+  }
+});
+
+app.post("/register", (req, res) => {
+  //use Destructuring
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+
+  res.json(database.users[database.users.length - 1]);
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("Not Found");
+  }
+});
+
+app.post("/image", (request, response) => {
+  const { id } = request.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return response.json(user.entries);
+    }
+  });
+  if (!found) {
+    response.status(400).json("Not Found");
+  }
+});
+
+// listen on port 3000 for requests
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
+});
+
+/*   
+   DESIGN OF THE ROUTES:
+   THESE ARE THE ENDPOINTS WE WILL CREATE:
+
+    / --> res = this is working
+    /signin -- POST = sucess/fail
+    /register --> POST = user
+    /profile/:userId --> GET = user
+    /image --> PUT --> user    //Updates the user object with a new image
+
+*/
